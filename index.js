740a7124d0e2df9bd52e00b85adea3f367f24a68
Tiago Souza <tiago.souza@zup.com.br>
2019-10-31 15:16:10 -0300

    Criar projeto

---
+++ index.js
@@ -0,0 +1,17 @@
+const  rapperModel = [
+    {
+        picture: "#",
+        name: "Djonga",
+        age: 26,
+        almbum: "Ladrão",
+        single: "Ladrão"        
+    },
+    {
+        picture: '#',
+        name: "Rapadura",
+        age: 30,
+        album: "RapBox",
+        single: "Norte Nordeste"
+
+    }
+]
