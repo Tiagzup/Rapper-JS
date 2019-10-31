2051ad3e415e3c59eb1f2b2dbbdb4ef5c091ff46
Tiago Souza <tiago.souza@zup.com.br>
2019-10-31 16:58:15 -0300

    Criar método create

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const  rapperModel = [
     {
         picture: "#",
@@ -15,3 +16,43 @@ const  rapperModel = [
 
     }
 ]
+let rapperAlive = rapperModel
+let rapperDead = []
+
+let rapperAliveHtml = []
+for (let i = 0; i < rapperAlive.length; i++) {
+    const rapper = rapperAlive[i];
+    rapperAliveHtml.push(`
+    <span>${rapper.picture.picture}</span><br>
+    <span>${rapper.picture.name}</span><br>
+    <span>${rapper.picture.age}</span><br>
+    <span>${rapper.picture.album}</span><br>
+    <span>${rapper.picture.single}</span><br>
+    <button onclick="matarRapper('${rapper.name}')"> Remover</button>
+    `)
+}
+
+const createRapper = () => {
+    const picture = document.getElementById("picture").value
+    const name = document.getElementById("name").value
+    const age = document.getElementById("age").value
+    const album = document.getElementById("album").value
+    const single = document.getElementById("single").value
+
+    rapperAlive.push({
+        picture,
+        name,
+        age,
+        album,
+        single
+    })
+
+    let rapperAliveHtml = []
+}
+
+/*rapperDead = rapperAlive.map(rapper => `<span>${rapper.picture}</span>
+<span>${rapper.name}</span>
+<span>${rapper.age}</span>
+<span>${rapper.album}</span>
+<span>${rapper.single}</span>`
+)
